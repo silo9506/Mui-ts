22e8b89dc1eeaf50bdf5f917899397df6e8f86bc
silo9506@gmail.com <95523501+silo9506@users.noreply.github.com>
2023-02-15 19:56:45 +0900

    ghpage

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { theme } from 'styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
